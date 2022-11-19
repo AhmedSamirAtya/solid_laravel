a4a80e321bc546e0d5a44deb805ff78982a45a96
ahmed mohamed <aa@gmail.com>
2022-11-19 23:17:49 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
-use App\Solid\OpenClosedPrinciple\Cat;
-use App\Solid\OpenClosedPrinciple\Communication;
-use App\Solid\OpenClosedPrinciple\Fox;
+use app\Solid\DependencyInversionPrinciple\Good\FawryGateway;
+use app\Solid\DependencyInversionPrinciple\Good\PayBalGateway;
+use app\Solid\DependencyInversionPrinciple\Good\PaymentProcess;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/pay-with-fawry', function () {
+    $paymentProcess = new PaymentProcess(new FawryGateway());
+
+   return $paymentProcess->pay();
+    
+});
+
+Route::get('/pay-with-paybal', function () {
+    $paymentProcess = new PaymentProcess(new PayBalGateway());
+
+   return $paymentProcess->pay();
+    
 });
